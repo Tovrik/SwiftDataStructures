c1e5895ccf1b212d36f59a78418422fdd39db442
Stefan Peterson <stefan.t.peterson@gmail.com>
2016-02-22 10:54:51 -0800

    fixed issue with Edge class

--- Graph/Edge.swift
+++ Graph/Edge.swift
@@ -6,8 +6,17 @@
 //
 //
 
-protocol Edge {
-    var origin: Int { get set }
-    var destination: Int { get set }
-    var weight: Int? { get set }
+public class Edge {
+    var origin: Int?
+    var destination: Int?
+    var weight: Int?
+    
+    init() {
+    }
+    
+    init(origin: Int?, destination: Int?, weight: Int?) {
+        self.origin = origin
+        self.destination = destination
+        self.weight = weight
+    }
 }
--- Graph/Graph.swift
+++ Graph/Graph.swift
@@ -6,8 +6,89 @@
 //
 //
 
-protocol Edge {
-    var origin: Int { get set }
-    var destination: Int { get set }
-    var weight: Int? { get set }
+
+public class Graph<T: Equatable> {
+    var vertices: [T]
+    var edges: [[Edge]] // our adjacency list
+    
+    public var vertexCount: Int { return vertices.count }
+    
+    init() {
+        self.vertices = [T]()
+        self.edges = [[Edge]]()
+    }
+    
+    init(vertices: [T]) {
+        self.vertices = [T]()
+        edges = [[Edge]]()
+        
+        for vertex in vertices {
+            self.addVertex(vertex)
+        }
+    }
+    
+    public func addVertex(v: T) {
+        vertices.append(v)
+        edges.append([Edge]())
+    }
+    
+    public func addEdge(e: Edge) {
+        if let org = e.origin {
+            edges[org].append(e)
+        }
+    }
+    
+    public func connectTwovertices(source: T, dest: T, weight: Int?) {
+        if let sourceIndex = getIndexForVertex(source) {
+            if let destIndex = getIndexForVertex(dest) {
+                addEdge(Edge(origin: sourceIndex, destination: destIndex, weight: weight ))
+            }
+        }
+    }
+    
+    public func getIndexForVertex(v: T) -> Int? {
+        if let index = vertices.indexOf(v) {
+            return index
+        }
+        return nil
+    }
+    
+    public func vertexAtIndex(index: Int) -> T {
+        return vertices[index]
+    }
+    
+    private func removeInRange(index: Int, left: Int, right: Int) {
+        for i in left..<right {
+            var indicesToRemove = [Int]()
+            for j in 0..<edges[i].count {
+                if edges[i][j].destination == index {
+                    indicesToRemove.append(j)
+                    continue
+                }
+                if edges[i][j].destination > index {
+                    if edges[i][j].destination != nil {
+                        edges[i][j].destination! -= 1
+                    }
+                }
+            }
+            for k in indicesToRemove {
+                edges[i].removeAtIndex(k)
+            }
+        }
+        
+    }
+    
+    public func removeVertex(v: T) {
+        if let index = getIndexForVertex(v) {
+            removeVertexAtIndex(index)
+        }
+    }
+    
+    public func removeVertexAtIndex(index: Int) {
+        removeInRange(index, left: 0, right: index)
+        removeInRange(index, left: index + 1, right: edges.count)
+        
+        edges.removeAtIndex(index)
+        vertices.removeAtIndex(index)
+    }
 }
